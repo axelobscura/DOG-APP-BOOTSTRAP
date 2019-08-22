992fbe7bc0caff85d0ce1938f1cd496fe759cb3a
Axel <axosar@gmail.com>
2019-08-22 09:28:36 -0500

    Refactoring Routes
    Refactoring Routes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-import DogList from './DogList';
-import DogDetails from './DogDetails';
-import { Switch, Route } from 'react-router-dom';
+import Routes from './Routes';
 import whiskey from './images/whiskey.jpg';
 import hazel from './images/hazel.jpg';
 import sham from './images/sham.jpg';
@@ -44,20 +42,10 @@ class App extends Component {
     ]
   }
   render(){
-    const getDog = props => {
-      let name = props.match.params.name;
-      let currentDog = this.props.dogs.find(
-        dog => dog.name.toLowerCase() === name.toLowerCase()
-      );
-      return <DogDetails {...props} dog={currentDog}/>
-    }
     return (
       <div>
         <NavBar dogs={this.props.dogs} />
-        <Switch>
-          <Route exact path='/dogs' render={() => <DogList dogs={this.props.dogs}/>} />
-          <Route exact path='/dogs/:name' render={getDog} />
-        </Switch>
+        {Routes}
       </div>
     );
   }
